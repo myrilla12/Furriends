b7a4430959fef79ec13c4459773825affcbe3344
kryst-ll <krystalleelhy@gmail.com>
2024-06-02 01:12:49 +0800

    added text for home page

--- furriends-fe/src/pages/index.tsx
+++ furriends-fe/src/pages/index.tsx
@@ -1,13 +1,27 @@
 import LoginButton from '@/components/loginButton';
 import LogoHeader from '@/components/logoHeader/logoHeader';
-import { Container, MantineProvider } from '@mantine/core';
+import { Container, MantineProvider, Space, Text, Title } from '@mantine/core';
 
 export default function Home() {
     return (
-        <>  
+        <>
             <MantineProvider>
                 <LogoHeader />
+                <Space h="xl" />
+
                 <Container>
+                    <Title order={3}>Welcome to{" "}
+                        <Text span c='#6d543e' inherit>Furriends</Text>
+                    </Title>
+
+                    <Space h="xs" />
+
+                    <Text component="p" size="md">
+                        A bespoke app for modern day pet owners.
+                    </Text>
+
+                    <Space h="lg" />
+
                     <LoginButton />
                 </Container>
             </MantineProvider>
