7eeb16c3c2f28dc47a9dca3a023679f9a5ded916
kryst-ll <krystalleelhy@gmail.com>
2024-06-02 23:01:05 +0800

    Change styling for logo & boxes

--- furriends-fe/tailwind.config.js
+++ furriends-fe/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
+    colours: {
+      'slate': colors.slate,
+    },
     extend: {},
   },
   plugins: [],
