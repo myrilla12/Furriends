ea53e76dc922d45289dd3d9fe300396c11141069
kryst-ll <krystalleelhy@gmail.com>
2024-06-08 01:22:31 +0800

    Fix styling issues in signout button & avatar

--- furriends-fe/src/components/signoutButton.tsx
+++ furriends-fe/src/components/signoutButton.tsx
@@ -17,7 +17,7 @@ export default function SignoutButton() {
 
     return (
         <button
-            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
             onClick={signout}
         >
             <PowerIcon className="w-6" />
