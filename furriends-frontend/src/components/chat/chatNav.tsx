7224a2eb489feabed202a4b233eab0a01ba7709d
Nasya <myrilla12@gmail.com>
2024-07-03 09:07:09 +0800

    Change colour of currently selected chat in ChatNav

--- furriends-frontend/src/components/chat/chatNav.tsx
+++ furriends-frontend/src/components/chat/chatNav.tsx
@@ -8,42 +8,48 @@ import type { User } from '@supabase/supabase-js'
 import { Box, Title } from '@mantine/core';
 import ChatIcon from './chatIcon';
 import { Chats, Profile } from '@/utils/definitions';
+import { useRouter } from 'next/router';
 
 type ChatNavProps = {
     chats: Chats,
 }
 
 export default function ChatNav({ chats } : ChatNavProps) {
-  function ChatCard() {
-    const pathname = usePathname();
+    function ChatCard() {
+        const router = useRouter();
+        const { pathname, query } = router;
+
+        return (
+        <>
+            {chats.map(obj => {
+                const isActive = query.id === obj.id;
+                return (
+                    <Link
+                        key={obj.otherUser.username}
+                        href={`/chat?id=${obj.id}`}
+                        className={clsx(
+                            "flex h-[70px] w-[400px] items-center justify-start gap-2 rounded-lg bg-gray-100 p-3 text-lg font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-3 md:px-6",
+                            { 
+                                'bg-amber-200': isActive,
+                                'bg-gray-100 hover:bg-sky-100 hover:text-blue-600': !isActive,
+                            },
+                        )}
+                    >
+                        <ChatIcon profile={obj.otherUser} />
+                        <p>{obj.otherUser.username}</p>
+                    </Link>
+                );
+            })}
+        </>
+        );
+    }
 
     return (
-      <>
-        {chats.map(obj => {
-            return (
-                <Link
-                key={obj.otherUser.username}
-                href={`/chat?id=${obj.id}`}
-                className={clsx(
-                    "flex h-[70px] w-[400px] items-center justify-start gap-2 rounded-lg bg-gray-100 p-3 text-lg font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-3 md:px-6",
-                    { 'bg-sky-50': pathname === '/dashboard' },
-                )}
-                >
-                    <ChatIcon profile={obj.otherUser} />
-                    <p>{obj.otherUser.username}</p>
-                </Link>
-            );
-        })}
-      </>
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+        <div className="flex grow flex-col justify-start space-y-2">
+            <ChatCard />
+        </div>
+        </div>
     );
-  }
-
-  return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-col justify-start space-y-2">
-        <ChatCard />
-      </div>
-    </div>
-  );
 }
 
