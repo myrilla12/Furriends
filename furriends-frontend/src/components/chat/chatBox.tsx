c43b5aa02c94d1184beb4e871c234b8652935cc8
Nasya <myrilla12@gmail.com>
2024-07-04 16:51:52 +0800

    Chatbox scrolling (auto-scroll to bottom)

--- furriends-frontend/src/components/chat/chatBox.tsx
+++ furriends-frontend/src/components/chat/chatBox.tsx
@@ -1,11 +1,10 @@
-import { Box, Button, Center, Container, Flex, Group, Input, ScrollArea, Text } from "@mantine/core";
+import { Box, Button, Container, Flex, Input, Text } from "@mantine/core";
 import { createClient } from "@/utils/supabase/component";
 import { useEffect, useRef, useState } from "react";
-import { RealtimeChannel, User } from "@supabase/supabase-js";
-import { GetServerSidePropsContext } from "next";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
 import { Message, Profile } from "@/utils/definitions";
 import styles from '../../styles/chatStyles.module.css';
-import { useRouter } from "next/router";
 import printTimestamp from "@/utils/printTimestamp";
 
 type ChatBoxProps = {
@@ -17,72 +16,84 @@ type ChatBoxProps = {
 
 export default function ChatBox({ user, chatId, messages, chatPartner }: ChatBoxProps) {
 
-    const supabase = createClient();
-    const router = useRouter();
-    const [message, setMessage] = useState<string>('');
+  const supabase = createClient();
+  const router = useRouter();
+  const scrollAreaRef = useRef<HTMLDivElement>(null);
+  const [message, setMessage] = useState<string>('');
 
-    async function sendMessage(content: string) {
-    // if chat id is null AKA it is a new chat (create new chat)
-        let currChatId = chatId;
-
-        if (currChatId === null) {
-            const { data: newChatData, error: newChatError } = await supabase
-                .from('chats')
-                .insert({})
-                .select('id')
-                .single();
-
-            if (newChatError) {
-                console.error('Error creating new chat:', newChatError);
-                return;
-            }
-
-            currChatId = newChatData.id;
-
-            // create new row in chat users for current user
-            const { data: userData, error: usersError } = await supabase
-                .from('chat_users')
-                .insert([
-                {
-                    chat_id: currChatId,
-                    user_id: user.id,
-                },
-                {
-                    chat_id: currChatId,
-                    user_id: chatPartner?.id,
-                }
-                ]);
-
-            if (usersError) {
-                console.error('Error storing user id in chat users:', usersError);
-            }
-
-            router.push(`/chat?id=${currChatId}`);
-        }
-
-        const messageData: Message = {
-            chat_id: currChatId,
-            author_id: user.id,
-            content: content,
-        }
-
-        // insert new message into the database
-        const { data, error } = await supabase
-            .from('messages')
-            .insert(messageData);
-
-        // update the updated_at value of 'chats'
-        const currentTimestamp = new Date().toISOString();
-
-        const { error: chatsError } = await supabase
-            .from('chats')
-            .update({ updated_at: currentTimestamp })
-            .eq('id', currChatId);
-
-        if (chatsError) {
-            console.error("Error updating the 'updated_at' column in 'chats':", chatsError);
-        }
+  const scrollToBottom = () => {
+    if (scrollAreaRef.current) {
+      scrollAreaRef.current.scrollTo({
+        top: scrollAreaRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
     }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  async function sendMessage(content: string) {
+    let currChatId = chatId;
+
+    if (currChatId === null) {
+      const { data: newChatData, error: newChatError } = await supabase
+        .from('chats')
+        .insert({})
+        .select('id')
+        .single();
+
+      if (newChatError) {
+        console.error('Error creating new chat:', newChatError);
+        return;
+      }
+
+      currChatId = newChatData.id;
+
+      const { data: userData, error: usersError } = await supabase
+        .from('chat_users')
+        .insert([
+          {
+            chat_id: currChatId,
+            user_id: user.id,
+          },
+          {
+            chat_id: currChatId,
+            user_id: chatPartner?.id,
+          }
+        ]);
+
+      if (usersError) {
+        console.error('Error storing user id in chat users:', usersError);
+      }
+
+      router.push(`/chat?id=${currChatId}`);
+    }
+
+    const messageData: Message = {
+      chat_id: currChatId,
+      author_id: user.id,
+      content: content,
+    }
+
+    const { data, error } = await supabase
+      .from('messages')
+      .insert(messageData);
+
+    const currentTimestamp = new Date().toISOString();
+
+    const { error: chatsError } = await supabase
+      .from('chats')
+      .update({ updated_at: currentTimestamp })
+      .eq('id', currChatId);
+
+    if (chatsError) {
+      console.error("Error updating the 'updated_at' column in 'chats':", chatsError);
+    }
+
+    scrollToBottom();
+  }
 
   const checkMessage = () => {
     if (message === '') {
@@ -90,49 +101,41 @@ export default function ChatBox({ user, chatId, messages, chatPartner }: ChatBox
       return false;
     }
     return true;
-  }
+  };
 
   return (
-    <Container
-      m='md'
-      className={`${styles.chatContainer}`}
-    >
+    <Container m='md' className={`${styles.chatContainer}`}>
       {/* Display author name + messages in chat bubbles */}
       <Box h={570}>
-        <ScrollArea h={550} pb='md' scrollbars="y">
+        <Box h={550} pb='md' ref={scrollAreaRef} style={{"overflow": "auto"}}>
           <div className="mt-5 flex flex-col gap-3">
             {messages?.map((msg, i) => (
-            <div
-              key={i}
-              className={`${styles.chatBubble} ${user.id === msg.author_id ? styles.user : styles.chatPartner}`}
-            >   
+              <div
+                key={i}
+                className={`${styles.chatBubble} ${user.id === msg.author_id ? styles.user : styles.chatPartner}`}
+              >
                 {user.id === msg.author_id ? 
                   (<Text size='sm' fw={700}>You</Text>) : 
                   (<Text size='sm' fw={700}>{chatPartner?.username}</Text>)
                 }
                 {msg.content}
-
-                <div style={{display: 'flex'}}>
-                    <Text mr='md' className={styles.chatTimestamp}>{msg.created_at ? printTimestamp(msg.created_at) : ''}</Text>
-                    {user.id === msg.author_id ? 
-                    msg.read_at === null ? (<Text fw={500} className={styles.chatTimestamp}>Unead</Text>) : 
-                        (<Text fw={500} className={styles.chatTimestamp}>Read</Text>) : ''
-                    }
+                <div style={{ display: 'flex' }}>
+                  <Text mr='md' className={styles.chatTimestamp}>
+                    {msg.created_at ? printTimestamp(msg.created_at) : ''}
+                  </Text>
+                  {user.id === msg.author_id ? 
+                    msg.read_at === null ? (<Text fw={500} className={styles.chatTimestamp}>Unread</Text>) : 
+                    (<Text fw={500} className={styles.chatTimestamp}>Read</Text>) : ''
+                  }
                 </div>
-            </div>
-          ))}
+              </div>
+            ))}
           </div>
-        </ScrollArea>
+        </Box>
       </Box>
-      
+
       {/* Input field + button to send messages */}
-      <Flex
-        gap="md"
-        justify="center"
-        align="flex-end"
-        direction="row"
-        wrap="wrap"
-      >
+      <Flex gap="md" justify="center" align="flex-end" direction="row" wrap="wrap">
         <Input
           type="text"
           placeholder="Message"
@@ -147,11 +150,10 @@ export default function ChatBox({ user, chatId, messages, chatPartner }: ChatBox
             }
           }}
         />
-        <Button size='lg' color='#ad925a' variant='filled'
-          className={styles.chatInput}
+        <Button size='lg' color='#ad925a' variant='filled' className={styles.chatInput}
           onClick={() => {
             if (checkMessage()) 
-            sendMessage(message).then(() => setMessage(''));
+              sendMessage(message).then(() => setMessage(''));
             }
           }
         >
