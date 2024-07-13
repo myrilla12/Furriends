31a97876beb1fe322985a00961ca26fbfa0a54e0
Nasya <myrilla12@gmail.com>
2024-07-13 17:49:54 +0800

    Change chatNav selected chat colour
    Changed colour to something more brown to fit the colour scheme

--- furriends-frontend/src/components/chat/chatNav.tsx
+++ furriends-frontend/src/components/chat/chatNav.tsx
@@ -26,7 +26,7 @@ export default function ChatNav({ chats } : ChatNavProps) {
                         className={clsx(
                             "flex h-[70px] w-[400px] items-center justify-start gap-2 rounded-lg bg-gray-100 p-3 text-lg font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-3 md:px-6",
                             { 
-                                'bg-amber-200': isActive,
+                                'bg-amber-900 bg-opacity-60': isActive,
                                 'bg-gray-100 hover:bg-sky-100 hover:text-blue-600': !isActive,
                             },
                         )}
