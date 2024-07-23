1879ddbec036139f27b524010f01884a85949ad5
kryst-ll <krystalleelhy@gmail.com>
2024-07-23 19:34:58 +0800

    Delete backend folder

--- furriends-backend/db/data/placeholder-data.js
+++
@@ -1,8 +0,0 @@
-const users = [
-    {
-        id: '410544b2-4001-4271-9855-fec4b6a6442a',
-        name: 'FurriendsAdmin',
-        email: 'admin@furriends.com',
-        password: 'furriends',
-    },
-];
