5d5bd87fe35ad88e1c2b09da60ce4959e980968f
kryst-ll <krystalleelhy@gmail.com>
2024-07-25 15:46:08 +0800

    Remove outdated image reference

--- src/components/chat/chatNotFound.tsx
+++ src/components/chat/chatNotFound.tsx
@@ -1,4 +1,3 @@
-// image from https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F4653086-cat-cartoon-outline&psig=AOvVaw1fPVGk5YKvF27tJIS9wL6l&ust=1719858655087000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLja9_L6g4cDFQAAAAAdAAAAABAE
 import { Container, Stack, Image, Title } from "@mantine/core";
 import styles from '../../styles/chatStyles.module.css';
 
