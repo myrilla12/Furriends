973cb6e1e9006899be235f46efb199d2ccbca6f4
kryst-ll <krystalleelhy@gmail.com>
2024-07-27 01:42:56 +0800

    Edit pets not found image

--- src/components/chat/chatNotFound.tsx
+++ src/components/chat/chatNotFound.tsx
@@ -7,24 +7,17 @@ import styles from '../../styles/chatStyles.module.css';
  * @returns {JSX.Element} The ChatNotFound component.
  */
 export default function ChatNotFound() {
-
-  return (
-    <Container
-      m='md'
-      className={`${styles.chatContainer}`}
-    >
-      <Stack
-        className="items-center justify-center h-full text-center"
-      >
-        <Image
-          src="/chat-not-found.png"
-          w={200}
-          alt="Outline of a dog"
-        />
-
-        <Title order={3} mx="sm" mt="sm">No chat selected.</Title>
-        <Title order={3} mx="sm" mb="sm">Please start a new chat or select an existing chat.</Title>
-      </Stack>
-    </Container>
-  );
+    return (
+        <Container m='md' className={`${styles.chatContainer}`}>
+            <Stack className="items-center justify-center h-full text-center">
+                <Image
+                    src="/chat-not-found.png"
+                    w={200}
+                    alt="Outline of a dog"
+                />
+                <Title order={3} mx="sm" mt="sm">No chat selected.</Title>
+                <Title order={3} mx="sm" mb="sm">Please start a new chat or select an existing chat.</Title>
+            </Stack>
+        </Container>
+    );
 }
