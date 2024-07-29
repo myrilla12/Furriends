78fdd88a9c2ad2bf8bbde5c4b2a9e8df5e596cc5
Nasya <myrilla12@gmail.com>
2024-07-29 11:02:12 +0800

    Update configs
    This version mostly works for both tests and application running. Until application has been run, tsconfig gets altered and tests no longer work. In the process of fixing this.

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: "ts-jest",
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
@@ -22,11 +21,11 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",  // Using ts-jest for TypeScript files
-    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: './babel-jest.config.js' }],
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!(@mantine|@tabler|@hello-pangea)/)" // Transforming specific packages if needed
+    "/node_modules/",
   ],
   testEnvironment: "jsdom", // Use "node" if not testing in a browser-like environment
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], 
